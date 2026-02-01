bb957f6a211409f5169665838a981fb6ba213a1a
Amy Corona <amy.m.corona@gmail.com>
2026-02-01 13:24:33 -0600

    Allow photo library selection in Home Remedies

--- src/app/sandbox/honeypot/home-remedies/page.tsx
+++ src/app/sandbox/honeypot/home-remedies/page.tsx
@@ -90,7 +90,6 @@ export default function HomeRemedies() {
               <input
                 type="file"
                 accept="image/*"
-                capture="environment"
                 onChange={handleImageChange}
                 className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 
                   file:rounded-full file:border-0 file:text-sm file:font-semibold 
